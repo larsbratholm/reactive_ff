8830b21659eba575401bd8de0cbb6ed7bfe53a21
Lars Andersen Bratholm <larsbratholm@gmail.com>
2020-10-29 14:04:15 +0000

    Added parsed dft files

--- unpack.sh
+++ unpack.sh
@@ -6,8 +6,15 @@ tar zxf archives/batch.tar.gz
 # Unpack batch_xyz.tar.gz
 tar zxf archives/batch_xyz.tar.gz
 
+# Unpack batch_dft.tar.gz
+tar zxf archives/batch_dft.tar.gz
+
+# Unpack exyz.tar.gz
+tar zxf archives/exyz.tar.gz
+
 # Gather and unpack xyz.tar.gz
 cat archives/xyz?? | tar zx
 
 # Gather and unpack log_files.tar.gz
 cat archives/log_files?? | tar zx
+
