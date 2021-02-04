fd6180e9489508e61ca03f38573823105dd8f056
Lars Andersen Bratholm <larsbratholm@gmail.com>
2021-02-04 17:50:12 +0000

    Updated unpacking script

--- unpack.sh
+++ unpack.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 
-# Unpack batch.tar.gz
-tar zxf archives/batch.tar.gz
+# Gather and unpack log_files.tar.gz
+# Q-Chem log-files Used as starting
+# point in normal mode sampling
+cat archives/log_files/log_files?? | tar zx
+
+# Gather and unpack xyz.tar.gz
+# Contains all the generated
+# structures from normal-mode sampling
+cat archives/xyz/xyz?? | tar zx
 
 # Unpack batch_xyz.tar.gz
+# xyz files of the first batch of
+# ~5000 structures
 tar zxf archives/batch_xyz.tar.gz
 
 # Unpack batch_dft.tar.gz
+# molpro output files of cheap
+# dft calculations on batch set
 tar zxf archives/batch_dft.tar.gz
 
-# Unpack exyz.tar.gz
-tar zxf archives/exyz.tar.gz
+# Unpack batch_hybrid.tar.bz2
+# molpro output files of double hybrid
+# calculations on batch set
+tar jxf archives/batch_hybrid.tar.bz2
 
-# Gather and unpack xyz.tar.gz
-cat archives/xyz?? | tar zx
+# Unpack exyz_dft.tar.gz
+# exyz files with cheap dft energies
+tar zxf archives/exyz_dft.tar.gz
 
-# Gather and unpack log_files.tar.gz
-cat archives/log_files?? | tar zx
-
+# Unpack exyz_hybrid.tar.gz
+# exyz files with cheap hybrid energies
+tar zxf archives/exyz_hybrid.tar.gz
